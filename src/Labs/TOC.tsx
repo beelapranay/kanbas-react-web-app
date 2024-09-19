a54fb07b1314ad025f18de1b3c4b83df82aff241
Pranay Beela <pranaybeela@pranays-mbp.mynetworksettings.com>
2024-09-19 16:49:34 -0400

    first commit

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,11 @@ export default function TOC() {
             <li><Link to="/Labs/Lab2">Lab 2</Link></li>
             <li><Link to="/Labs/Lab3">Lab3</Link></li>
             <li><Link to="/Kanbas">Kanbas</Link></li>
+            <li><Link
+                to="https://github.com/beelapranay/kanbas-react-web-app/tree/main" id="wd-github"
+                target="_black" rel="noopener noreferrer">
+                    GitHub
+            </Link></li>
         </ul>
     )
 }
