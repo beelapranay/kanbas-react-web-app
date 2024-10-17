a91c234087ceb888ae73ed192bb8916af8b5a748
Pranay Beela <pranaybeela@mac.mynetworksettings.com>
2024-10-17 18:59:50 -0400

    added some changes

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -76,7 +76,6 @@ export default function Lab3() {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
                 vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
             </Highlight>
-            <AddPathParameters />
             <PathParameters />
 
         </div>
